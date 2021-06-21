a60007729b58fb15809cbe310c2814dca8e1dd52
Jorgen <ybeliaev@gmail.com>
2021-06-21 18:38:39 +0300

    add prettier and linter and header footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
-  return (
-    <>
-      <h1>Wellcome!</h1>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <h1>Wellcome!</h1>
+            </main>
+            <Footer />
+        </>
+    )
 }
 
-export default App;
+export default App
