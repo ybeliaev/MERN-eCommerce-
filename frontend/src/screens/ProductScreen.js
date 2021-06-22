6d1728164654d026152b9f5c472f9576463f5bc0
Jorgen <ybeliaev@gmail.com>
2021-06-22 17:35:33 +0300

    ProductScreen : image description price etc

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,77 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 
-const ProductScreen = () => {
-    return <div>Product</div>
+import Raiting from '../components/Raiting'
+import products from '../products'
+
+const ProductScreen = (props) => {
+    const product = products.find((p) => p._id === props.match.params.id)
+    console.log('props from props ProductScreen: ', props)
+    return (
+        <>
+            <Link className="btn btn-light my-3" to="/">
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Raiting
+                                value={product.value}
+                                text={`${product.numReviews} rewiews`}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>Price: {product.price}</ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: ${product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col className="md-3">
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        <strong>
+                                            {product.countInStock > 0
+                                                ? 'In Stock'
+                                                : 'Out Of Stock'}
+                                        </strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button
+                                    className="btn w-100"
+                                    type="button"
+                                    disabled={product.countInStock === 0}
+                                >
+                                    Add to Card
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    )
 }
 
 export default ProductScreen
