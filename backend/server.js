7aecec0e7b3bbf685698991900f1a2a3757099b5
Jorgen <ybeliaev@gmail.com>
2021-06-28 13:41:47 +0300

    del modules from package - error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const products = require('./data/products')
 
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import products from './data/products'
+
+console.log(products)
+
 dotenv.config()
 
 const app = express()
