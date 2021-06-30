78a28be5804e4ff1d9b71c95ca6884fd7d35e80e
Jorgen <ybeliaev@gmail.com>
2021-06-30 14:03:04 +0300

    connectDB create + corrected module export

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+// const express = require('express')
+// const dotenv = require('dotenv')
+// const products = require('./data/products')
 
-// import express from 'express'
-// import dotenv from 'dotenv'
-// import products from './data/products'
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from '../backend/data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
