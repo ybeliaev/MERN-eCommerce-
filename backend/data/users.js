7017ca6127bd714189cc82c9168475333f763116
Jorgen <ybeliaev@gmail.com>
2021-07-10 16:13:42 +0300

    users.js added + i bcryptjs

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxxx,
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: xxxxx,
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        password: xxxxx,
+    },
+]
