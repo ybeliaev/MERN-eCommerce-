40f5215f52a88a76634edbb38b4d69e075e558dd
Jorgen <ybeliaev@gmail.com>
2021-07-10 16:17:36 +0300

    bcrypt.hashSync() added

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,22 @@
+import bcrypt from 'bcryptjs'
+
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: xxxxx,
+        password: bcrypt.hashSync('123456', 10),
         isAdmin: true,
     },
     {
         name: 'John Doe',
         email: 'john@example.com',
-        password: xxxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
     {
         name: 'Jane Doe',
         email: 'jane@example.com',
-        password: xxxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
 ]
+
+export default users
