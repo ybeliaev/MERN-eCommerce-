1d39eed8949c495f62f097c96e7bbd1360fff800
Jorgen <ybeliaev@gmail.com>
2021-07-17 17:26:12 +0300

    check error 404 & 500

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,31 @@ connectDB()
 
 const app = express()
 
+// app.use((req, res, next) => {
+//     console.log(req.originalUrl) // /api/products if request for all products
+//     next()
+// })
+
 app.get('/', (req, res) => {
     res.send('API is running!..')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error(`NOT FOUND - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    })
+})
 
 const PORT = process.env.PORT || 5555
 
