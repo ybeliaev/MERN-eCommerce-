8be72a96002814ef7f122195f998d0abb991302e
Jorgen <ybeliaev@gmail.com>
2021-07-17 13:34:56 +0300

    express-async-handler add

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+import Product from '../models/productModel'
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    const product = await Product.find({})
+    res.json(products)
+})
+
+router.get('/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
-import products from '../backend/data/products.js'
+// import products from '../backend/data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -18,14 +20,7 @@ app.get('/', (req, res) => {
     res.send('API is running!..')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use('./api/products', productRoutes)
 
 const PORT = process.env.PORT || 5555
 
